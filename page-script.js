935cea6e77fe42bbf9692da169d6b317adb9545f
DocterBuster <87544221+DocterBuster@users.noreply.github.com>
2025-09-17 11:17:14 -0400

    Page template updated

---
+++ page-script.js
@@ -0,0 +1,18 @@
+
+
+// A generic page script to be used in all pages to add various effects 
+
+
+console.log("page-script.js Loaded!")
+
+
+
+
+//Add event listneners to all project cards
+
+const projectCards = document.querySelectorAll('.project-card');
+
+for (const projectCard of projectCards)
+{
+    
+}
